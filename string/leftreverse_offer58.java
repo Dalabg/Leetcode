65e12949cc60092c48b3c49e6037b5da5f6a7860
Dalabg <1137862826@qq.com>
2023-08-31 23:37:46 +0800

    push over

--- string/leftreverse_offer58.java
+++ string/leftreverse_offer58.java
@@ -1,5 +1,13 @@
 package Coding_Caprice.string;
 
 public class leftreverse_offer58 {
-    
+    public String reverseLeftWords(String s, int n) {
+        int len = s.length();
+        int p = n%len;
+        if(p==0){
+            return s;
+        }
+        String res = s.substring(p)+s.substring(0,p);
+        return res;
+    }
 }
--- string/stringtest.java
+++ string/stringtest.java
@@ -2,8 +2,10 @@ package Coding_Caprice.string;
 
 public class stringtest {
     public static void main(String[] args) {
-        reversestr_541 r = new reversestr_541();
-        System.out.println(r.reverseStr("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc", 20).equals("jlnnxsetgcpsbhsfymrkhfursyissjnsocgdhgfxtxrlvugsaphqzxllwebukgatzfybprfmmfithphccxfsogsgqsnvckjvnskk"));
-        System.out.println("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc".length());
+//        reversestr_541 a = new reversestr_541();
+//        System.out.println(a.reverseStr("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc", 20).equals("jlnnxsetgcpsbhsfymrkhfursyissjnsocgdhgfxtxrlvugsaphqzxllwebukgatzfybprfmmfithphccxfsogsgqsnvckjvnskk"));
+//        System.out.println("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc".length());
+        leftreverse_offer58 b = new leftreverse_offer58();
+        System.out.println(b.reverseLeftWords("asdfgh",2));
     }
 }
